9247c95f8367a4e820eb8d979726bb30e2cf1abc
dirrectZ <matei.robescu@gmail.com>
2024-11-07 22:10:35 +0200

    start

--- VF&VM/main.c
+++ VF&VM/main.c
@@ -1,14 +1,10 @@
-//
-//  main.c
-//  VF&VM
-//
-//  Created by Matei Robescu on 07.11.2024.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, const char * argv[])
+{
+    
+    
+    
     return 0;
 }
+
