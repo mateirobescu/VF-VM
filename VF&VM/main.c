9112074a24b9a5e3d5b9754bf7000fe5177646bf
dirrectZ <matei.robescu@gmail.com>
2024-11-07 22:02:34 +0200

    Initial Commit

---
+++ VF&VM/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  VF&VM
+//
+//  Created by Matei Robescu on 07.11.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
